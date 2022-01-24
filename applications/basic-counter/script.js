9f19861ac452e60111497a15f620107592e6086d
Michael Rico <rico.michael@gmail.com>
2022-01-24 15:55:20 -0800

    basic counter exc 2

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,10 +1,28 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import {
+  fromEvent,
+  interval,
+  merge,
+  NEVER,
+  skipUntil,
+  takeUntil,
+  scan,
+  map,
+} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 // observables created for dom nodes
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
+let timer$ = interval(1000).pipe(
+  skipUntil(start$),
+  scan((acc, curr) => {
+    return acc + 1;
+  }, 0),
+  takeUntil(pause$),
+);
+
+timer$.subscribe(setCount);
 
 // subscribe to start$
 // -- create timer
@@ -12,12 +30,12 @@ const pause$ = fromEvent(pauseButton, 'click');
 
 // pause, unsubscribe
 
-let timer$ = interval(1000);
-let tSub;
-start$.subscribe((x) => {
-  tSub = timer$.subscribe(setCount);
-});
+// let timer$ = interval(1000);
+// let tSub;
+// start$.subscribe((x) => {
+//   tSub = timer$.subscribe(setCount);
+// });
 
-pause$.subscribe(() => {
-  tSub.unsubscribe()
-});
+// pause$.subscribe(() => {
+//   tSub.unsubscribe()
+// });
