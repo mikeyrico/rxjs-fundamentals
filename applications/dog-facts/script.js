91a6952c9653937d18564f5ddac98f176284bf86
Michael Rico <rico.michael@gmail.com>
2022-01-24 17:42:27 -0800

    basic fetch with retry

--- applications/dog-facts/script.js
+++ applications/dog-facts/script.js
@@ -21,4 +21,35 @@ import {
   setError,
 } from './utilities';
 
-const endpoint = 'http://localhost:3333/api/facts';
+// const endpoint = 'http://localhost:3333/api/facts?delay=2000&chaos=true&flakiness=1';
+export const endpoint = 'http://localhost:3333/api/facts';
+
+const fetch$ = fromEvent(fetchButton, 'click').pipe(
+  exhaustMap(() => {
+    return fromFetch(endpoint).pipe(
+      tap(clearError),
+      mergeMap((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something blew up');
+        }
+      }),
+      retry(4),
+      catchError((error) => {
+        return of({ error: error.message });
+      }),
+    );
+  }),
+);
+
+fetch$.subscribe((payload) => {
+  const { facts, error } = payload;
+  // console.log('payload', payload)
+  // console.log('error', error)
+  if (error) {
+    return setError(error);
+  }
+  clearFacts();
+  addFacts(payload);
+});
