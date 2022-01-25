ab18492d17a139416ee537b9e058303669e86176
Michael Rico <rico.michael@gmail.com>
2022-01-24 20:37:46 -0800

    delay loading indicator states

--- applications/loading-states/script.js
+++ applications/loading-states/script.js
@@ -10,3 +10,25 @@ import {
   form,
   fetchData,
 } from './utilities';
+
+const showLoading$ = of(true).pipe(
+  delay(+showLoadingAfterField.value),
+  tap(() => showLoading(true)),
+);
+
+const hideLoading$ = of(true).pipe(
+  delay(+showLoadingForAtLeastField.value),
+  tap(() => showLoading(false)),
+);
+
+const loading$ = fromEvent(form, 'submit').pipe(
+  // before
+  // tap(() => showLoading(true)),
+  // exhaustMap(() => fetchData()),
+  // tap(() => showLoading(false))
+
+  // after with show and hide loading observables above
+  exhaustMap(() => concat(showLoading$, fetchData(), hideLoading$)),
+);
+
+loading$.subscribe();
