04642076891b561f88fa20ff02be1b088ef8281f
Michael Rico <rico.michael@gmail.com>
2022-01-24 20:20:03 -0800

    api fetch with loading of additional data

--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -1,4 +1,5 @@
 import {
+  first,
   debounceTime,
   distinctUntilChanged,
   fromEvent,
@@ -26,25 +27,54 @@ import {
   endpointFor,
   search,
   form,
+  renderPokemon,
 } from '../pokemon/utilities';
 
-const endpoint = 'http://localhost:3333/api/pokemon/search/';
+const endpoint = 'http://localhost:3333/api/pokemon/';
+
+const searchPokemon = (searchTerm) => {
+  return fromFetch(
+    endpoint + 'search/' + searchTerm + '?delay=1000&chaos=true',
+  ).pipe(mergeMap((res) => res.json()));
+};
+
+const getPokemonData = (pokemon) => {
+  return fromFetch(endpoint + pokemon.id).pipe(mergeMap((res) => res.json()));
+};
+
+const search$ = fromEvent(form, 'submit').pipe(
+  map(() => search.value),
+  switchMap(searchPokemon),
+  pluck('pokemon'),
+  mergeMap((pokemon) => pokemon),
+  first(),
+  tap(renderPokemon),
+  switchMap((pokemon) => {
+    const pokemon$ = of(pokemon);
+    const additionalData$ = getPokemonData(pokemon).pipe(
+      map((data) => ({
+        ...pokemon,
+        data,
+      })),
+    );
+    return merge(pokemon$, additionalData$);
+  }),
+  tap(renderPokemon),
+  tap(console.log),
+);
 
 // autocomplete in rxjs
-const search$ = fromEvent(search, 'input').pipe(
+const oldSearch$ = fromEvent(search, 'input').pipe(
   debounceTime(300),
   // map(evt => evt.target.value),
   map((evt) => evt.target.value),
   distinctUntilChanged(),
-  switchMap((searchTerm) => {
-    return fromFetch(endpoint + searchTerm + '?delay=1000&chaos=true').pipe(
-      mergeMap((res) => res.json()),
-    );
-  }),
+  searchPokemon,
   tap(clearResults),
   tap(console.log),
   pluck('pokemon'),
   tap(addResults),
 );
 
+// oldSearch$.subscribe();
 search$.subscribe();
